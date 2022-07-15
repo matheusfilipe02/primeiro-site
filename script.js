601253ef4918cce10d8fa78286413cd2e8156f42
Matheus Filipe de Lima Silva <108430464+matheusfilipe02@users.noreply.github.com>
2022-07-15 10:07:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function enviar() {
+    let name = document.getElementById("Ntext")
+    let mail = document.getElementById("Mail")
+    let password = document.getElementById("Senha")
+    if (name.value.length == "" || mail.value.length == "" || password.value.length == "") {
+        alert("Por favor, digite o que se pede!")
+    }
+}
