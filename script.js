2d4dfea5780d4d481476a2e1b10b16d09a272a4d
matheusfilipe02 <malipesilva@outlook.com>
2022-07-22 14:02:04 -0300

    commit

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function enviar() {
-    let name = document.getElementById("Ntext")
     let mail = document.getElementById("Mail")
     let password = document.getElementById("Senha")
-    if (name.value.length == "" || mail.value.length == "" || password.value.length == "") {
+    if (mail.value.length == "" || password.value.length == "") {
         alert("Por favor, digite o que se pede!")
-    } else {
-        alert("Cadastro efetuado com sucesso!")
     }
 }
